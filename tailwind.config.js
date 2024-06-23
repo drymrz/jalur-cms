3fce13d6dd1864615766e14a42fd8822a336196f
Adry Mirza <adrymirza@gmail.com>
2024-06-23 15:48:37 +0700

    chore: Add initial project files and configurations, install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/views/**/*.blade.php"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Helvetica Neue", "Arial", "sans-serif"],
+            },
+            colors: {
+                jalur: {
+                    base: "#74B0AA",
+                    bold: "#61938E",
+                    bold2: "#4D7571",
+                    dark: "#0D544D",
+                },
+            },
+        },
+    },
+    plugins: [],
+};

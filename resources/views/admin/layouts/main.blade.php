36fca335824478534349dc60a8d7357776494897
Adry Mirza <adrymirza@gmail.com>
2025-02-01 12:09:31 +0700

    fix: Add Content Security Policy meta tag to enhance security

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @php
         $webSetting = App\Models\WebSetting::first();
     @endphp
@@ -21,7 +22,6 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 
 <body class=" bg-white [&.dark]:bg-main-dark font-jost relative text-[15px] font-normal leading-[1.5] m-0 p-0">
